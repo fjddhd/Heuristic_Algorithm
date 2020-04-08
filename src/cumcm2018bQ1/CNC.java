b1a59fdf536be31b1eedc7f562c1c40310d59e5c
多多 <499838742@qq.com>
2020-04-08 21:15:34 +0800

    cumcm2018-B-Q1

---
+++ src/cumcm2018bQ1/CNC.java
@@ -0,0 +1,89 @@
+package cumcm2018bQ1;
+
+public class CNC {//0~7号
+
+    public CNC(int CNCnum, int RGVposition,int fastTime,int slowTime,int workTime) {
+        this.CNCnum = CNCnum;
+        this.CNCposition = CNCnum / 2;
+        this.type = 1;
+        this.udTime=(CNCnum%2==0?fastTime:slowTime);
+        this.workTime=workTime;
+        int step = Math.abs(this.CNCposition - RGVposition);
+        if (step == 0) {
+            this.addressTime = 0+udTime;
+        } else if (step == 1) {
+            this.addressTime = RGV.stepT1+udTime;
+        } else if (step == 2) {
+            this.addressTime = RGV.stepT2+udTime;
+        } else if (step == 3) {
+            this.addressTime = RGV.stepT3+udTime;
+        }else {
+            System.err.println("cumcm2018bQ1.CNC：步长计算出错,程序设计出错");
+        }
+    }
+    public void update(int preTime,int newTime,int NewPos){//注意，该方法传入的是RGV完成该操作后的位置
+        if (type==1 ||type==2){
+            int step = Math.abs(this.CNCposition - NewPos);
+            if (step == 0) {
+                addressTime = 0+udTime;
+            } else if (step == 1) {
+                addressTime = RGV.stepT1+udTime;
+            } else if (step == 2) {
+                addressTime = RGV.stepT2+udTime;
+            } else if (step == 3) {
+                addressTime = RGV.stepT3+udTime;
+            }else {
+                System.err.println("cumcm2018bQ1.CNC：步长计算出错,程序设计出错");
+            }
+            System.out.println(CNCnum+" 号CNC未上过料，剩余时间为"+addressTime);
+        }else {//type==0,加工（或清洗）中
+            if (addressTime-(newTime-preTime)>udTime){//当前处理时间减去该操作更新时间大于上下料时间，表示还未加工完
+                addressTime-=(newTime-preTime);
+                System.out.println(CNCnum+" 号CNC正在加工中，减去上步时间："+(newTime-preTime)+",剩余时间为"+addressTime);
+            }else {//加工完了，按照移动距离耗时和下料时间计时，以下算是代替了原设定的状态2：下料等待的状态刷新
+                int step = Math.abs(this.CNCposition - NewPos);
+                if (step == 0) {
+                    addressTime = 0+udTime;
+                } else if (step == 1) {
+                    addressTime = RGV.stepT1+udTime;
+                } else if (step == 2) {
+                    addressTime = RGV.stepT2+udTime;
+                } else if (step == 3) {
+                    addressTime = RGV.stepT3+udTime;
+                }else {
+                    System.err.println("cumcm2018bQ1.CNC：步长计算出错,程序设计出错");
+                }
+                System.out.println(CNCnum+" 号CNC正在加工完成，剩余时间为"+addressTime);
+//                type=2;
+            }
+        }
+    }
+    public void upLoad(int newTime,Boolean isNeedClean){//传入时间为该操作刚开始时间
+        System.out.println("第"+CNCnum+"号CNC正在上料");
+        mm=new MyMaterial(newTime,CNCnum);
+        RGV.Upmaterials.add(mm);
+        type=0;
+        addressTime=workTime+2*udTime;//RGV在当前位置
+    }
+    public void downLoad(int newTime){//传入时间为该操作刚开始时间
+        System.out.println("第"+CNCnum+"号CNC正在下料");
+        mm.downTime=newTime;//下料开始时间
+        if (mm.downTime+udTime+25<=8*3600) {
+            RGV.Upmaterials.remove(mm);
+            RGV.Downmaterials.add(mm);
+        }
+        upLoad(newTime,true);
+    }
+    public void clean(){//指清洗由该cnc刚下来的物料
+    }
+
+    int CNCnum;
+    int CNCposition;
+    int type;//0,1,2。0表示加工中，addressTime为当前位置到该cnc位置移动时间+加工剩余时间+上（下）料时间；
+    // 1,2表示上下料，addressTime当前位置到该cnc位置移动时间+上（下）料时间
+    //1仅在从未上过料时存在，因为之后的上料都是在下料后马上上料,type一直保持为0
+    int addressTime;//距离最快上料完成或距离最快下料完成时间
+    int udTime;
+    int workTime;
+    MyMaterial mm;
+}
---
+++ src/cumcm2018bQ1/RGV.java
@@ -0,0 +1,245 @@
+package cumcm2018bQ1;
+
+import java.util.ArrayList;
+import java.util.List;
+public class RGV {//所有时间采用“秒”单位
+    public static int CurrentTime;//-TODO 每次操作需要更新
+    public static int RGVposition;//0~3，//-TODO 每次移动操作需要更新
+    public static List<CNC> SignialQueue;//-TODO 每次操作需要更新
+    public static List<MyMaterial> Upmaterials;//使用添加MyMaterial并执行其构造函数表示该物料处于加工中 -TODO 每次操作需要更新
+    public static List<MyMaterial> Downmaterials;//调用完downThis后添加进去，用于记录
+    public static Boolean callClean=false;
+    public static int stepT1=20;
+    public static int stepT2=33;
+    public static int stepT3=46;
+    public static int fastT=28;
+    public static int slowT=31;
+    public static int workT=200;
+    public static int cleanT=25;
+    public static void initDatas(){
+        CurrentTime=0;
+        RGVposition = 0;
+        SignialQueue=new ArrayList<>();
+        for (int i = 0; i < 8; ++i) {
+            SignialQueue.add(new CNC(i,RGVposition,fastT,slowT,workT));
+        }
+        Upmaterials=new ArrayList<>();
+        Downmaterials=new ArrayList<>();
+    }
+    public static void Assignment(int MaxTime){//位置贪心，CNC贪心
+        int opsCount=0;
+        while (CurrentTime<=MaxTime){//每次循环RGV进行一次操作并进行更新，
+            //按优先级选择对应操作，每次操作后更新
+            System.out.println("-------当前时间："+CurrentTime+",第"+(++opsCount)+"次操作开始：");
+            if (callClean){
+                System.out.println("====清洗操作====");
+                callClean=false;
+                update(cleanT,-1);
+                continue;
+            }
+            int nearPos= findNearestPos();
+            System.out.println("最近位置："+(nearPos)+"  当前位置："+RGVposition);
+            if (RGVposition==nearPos){
+                //选择该位置上时间短的处理
+                if (SignialQueue.get(RGVposition*2).addressTime<=SignialQueue.get(RGVposition*2+1).addressTime){//同位置快CNC等待时间短
+                    if (SignialQueue.get(RGVposition*2).addressTime>fastT){//该CNC还在加工，需执行等待操作
+                        update(SignialQueue.get(RGVposition*2).addressTime-fastT,-1);
+                    }else {//上下料
+                        int opCNCtype=SignialQueue.get(RGVposition*2).type;
+                        if (opCNCtype==1){//还未上过料，只上料
+                            SignialQueue.get(RGVposition*2).upLoad(CurrentTime,false);
+//                            CurrentTime+=28;
+                            update(fastT,-1);
+                            continue;
+                        }else if (opCNCtype==0){//下料，且downLoad方法已经有了上料过程
+                            SignialQueue.get(RGVposition*2).downLoad(CurrentTime);
+//                            CurrentTime+=28;
+                            callClean=true;
+                            update(fastT,-1);
+                            continue;
+                        }else {
+                            System.out.println("程序出错");
+                            break;
+                        }
+                    }
+                }else {//同位置慢CNC等待时间短
+                    if (SignialQueue.get(RGVposition*2+1).addressTime>slowT){//需执行等待操作
+                        update(SignialQueue.get(RGVposition*2+1).addressTime-slowT,-1);
+                    }else {//上下料
+                        int opCNCtype=SignialQueue.get(RGVposition*2+1).type;
+                        if (opCNCtype==1){//还未上过料，只上料
+                            SignialQueue.get(RGVposition*2+1).upLoad(CurrentTime,false);
+//                            CurrentTime+=31;
+                            update(slowT,-1);
+                            continue;
+                        }else if (opCNCtype==0){//下料，且downLoad方法已经有了上料过程
+                            SignialQueue.get(RGVposition*2+1).downLoad(CurrentTime);
+//                            CurrentTime+=31;
+                            callClean=true;
+                            update(slowT,-1);
+                            continue;
+                        }else {
+                            System.out.println("程序出错");
+                            break;
+                        }
+                    }
+                }
+            }else {//总和时间上位置与当前位置不一致，需执行移动操作
+                int step=Math.abs(nearPos-RGVposition);
+                int stepTime=0;
+                if (step==1){
+                    stepTime=stepT1;
+                }else if (step==2){
+                    stepTime=stepT2;
+                }else if (step==3){
+                    stepTime=stepT3;
+                }else {
+                    System.out.println("程序出错！");
+                    break;
+                }
+                update(stepTime,nearPos);
+            }
+        }
+    }
+    public static void Assignment2(int MaxTime){//只对CNC贪心
+        int opsCount=0;
+        int allWaitTime=0;
+        while (CurrentTime<=MaxTime){//每次循环RGV进行一次操作并进行更新，
+            //按优先级选择对应操作，每次操作后更新
+            System.out.println("-------当前时间："+CurrentTime+",第"+(++opsCount)+"次操作开始：");
+            if (callClean){
+                System.out.println("====清洗操作====");
+                callClean=false;
+                update(cleanT,-1);
+                continue;
+            }
+            int nearCNC= findNearestCNC();
+            int posOfNearCNC=nearCNC/2;
+            System.out.println("最近CNC："+(nearCNC)+"  当前位置："+RGVposition);
+            if (RGVposition==posOfNearCNC){
+                //选择该位置上时间短的处理
+                if (nearCNC%2==0){//同位置快CNC等待时间短
+                    if (SignialQueue.get(nearCNC).addressTime>fastT){//该CNC还在加工，需执行等待操作
+                        allWaitTime+=SignialQueue.get(nearCNC).addressTime-fastT;
+                        update(SignialQueue.get(nearCNC).addressTime-fastT,-1);
+                    }else {//上下料
+                        int opCNCtype=SignialQueue.get(nearCNC).type;
+                        if (opCNCtype==1){//还未上过料，只上料
+                            SignialQueue.get(nearCNC).upLoad(CurrentTime,false);
+//                            CurrentTime+=28;
+                            update(fastT,-1);
+                            continue;
+                        }else if (opCNCtype==0){//下料，且downLoad方法已经有了上料过程
+                            SignialQueue.get(nearCNC).downLoad(CurrentTime);
+//                            CurrentTime+=28;
+                            callClean=true;
+                            update(fastT,-1);
+                            continue;
+                        }else {
+                            System.out.println("程序出错");
+                            break;
+                        }
+                    }
+                }else {//同位置慢CNC等待时间短
+                    if (SignialQueue.get(nearCNC).addressTime>slowT){//需执行等待操作
+                        allWaitTime+=SignialQueue.get(nearCNC).addressTime-slowT;
+                        update(SignialQueue.get(nearCNC).addressTime-slowT,-1);
+                    }else {//上下料
+                        int opCNCtype=SignialQueue.get(nearCNC).type;
+                        if (opCNCtype==1){//还未上过料，只上料
+                            SignialQueue.get(nearCNC).upLoad(CurrentTime,false);
+//                            CurrentTime+=31;
+                            update(slowT,-1);
+                            continue;
+                        }else if (opCNCtype==0){//下料，且downLoad方法已经有了上料过程
+                            SignialQueue.get(nearCNC).downLoad(CurrentTime);
+//                            CurrentTime+=31;
+                            callClean=true;
+                            update(slowT,-1);
+                            continue;
+                        }else {
+                            System.out.println("程序出错");
+                            break;
+                        }
+                    }
+                }
+            }else {//总和时间上位置与当前位置不一致，需执行移动操作
+                int step=Math.abs(posOfNearCNC-RGVposition);
+                int stepTime=0;
+                if (step==1){
+                    stepTime=stepT1;
+                }else if (step==2){
+                    stepTime=stepT2;
+                }else if (step==3){
+                    stepTime=stepT3;
+                }else {
+                    System.out.println("程序出错！");
+                    break;
+                }
+                update(stepTime,posOfNearCNC);
+            }
+        }
+        System.out.println("总共等待时间："+allWaitTime);
+    }
+    public static int findNearestPos(){
+        int[] posRemainTime=new int[4];
+        for (int i=0;i<SignialQueue.size();++i){
+            posRemainTime[i/2]+=SignialQueue.get(i).addressTime;
+        }
+        int minValue=Integer.MAX_VALUE;
+        for (int i=0;i<4;++i){
+            minValue=Math.min(minValue,posRemainTime[i]);
+        }
+        int i;
+        for (i=0;i<4;++i){
+            if (posRemainTime[i]==minValue){
+                break;
+            }
+        }
+        return i;
+    }public static int findNearestCNC(){//返回时间最小的CNC
+        int minTime=Integer.MAX_VALUE;
+        int i;
+        for (i=0;i<SignialQueue.size();++i){
+            minTime=Math.min(minTime,SignialQueue.get(i).addressTime);
+        }
+        for (i=0;i<SignialQueue.size();++i){
+            if (SignialQueue.get(i).addressTime==minTime){
+                break;
+            }
+        }
+        return i;
+    }
+    /**
+     * 等待或移动操作发起
+     * 2.moveToPos:移动操作需要填写0~3这些位置，非移动操作填-1表示该操作步不移动
+     */
+    public static void update(int stepTime,int moveToPos){
+        System.out.println("==============CNC更新================");
+        int preTime=CurrentTime;
+        CurrentTime+=stepTime;
+        if (moveToPos==-1){
+            System.out.println("等待或因上下料清洗而刷新CNC状态："+stepTime+"秒");
+        }else {
+            System.out.println("从位置"+RGVposition+"移动到"+moveToPos+",用时："+stepTime+"秒");
+            RGVposition=moveToPos;
+        }
+        for (int i=0;i<SignialQueue.size();++i){
+            SignialQueue.get(i).update(preTime,CurrentTime,moveToPos==-1?RGVposition:moveToPos);//注意，该方法传入的是RGV完成该操作后的位置
+        }
+    }
+
+    public static void main(String[] args) {
+        initDatas();
+        Assignment2(8*3600);
+        for (int i=0;i<Downmaterials.size();++i){
+            System.out.println("第"+i+"个物料数据如下：");
+            System.out.println("所加工CNC编号："+Downmaterials.get(i).AssignedCNC);
+            System.out.println("上料开始时间："+Downmaterials.get(i).upTime);
+            System.out.println("下料开始时间："+Downmaterials.get(i).downTime);
+            System.out.println("========================================================");
+        }
+        System.out.println("总共完成物料 "+Downmaterials.size()+"件");
+        System.out.println("断点");
+    }
+}
